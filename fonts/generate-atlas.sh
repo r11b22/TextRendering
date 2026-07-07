d66475c7e8bd661897f9a8fce5876529bce616e1
r11b22 <joost.lehaen@gmail.com>
2026-07-07 21:27:56 +0200

    Made raw texture text rendering with uvs, not the correct width yet

---
+++ fonts/generate-atlas.sh
@@ -0,0 +1,8 @@
+msdf-atlas-gen \
+  -font font-sources/FreeSans.otf \
+  -type sdf \
+  -imageout font-atlas/FreeSans.png \
+  -json font-atlas/FreeSans.json \
+  -pxalign horizontal \
+  -dimensions 512 512 \
+  -charset charset.txt
